87f7114fb8979489d0cb2cc43d4692729086a32c
zhenyu <zhu.zheny@husky.neu.edu>
2018-01-30 16:35:08 -0500

    gateway

--- infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
@@ -3,4 +3,6 @@
 STACK_NAME=$1
 
 VPC_ID=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 | jq -r '.Vpc.VpcId')
-aws ec2 create-tags --resources $VPC_ID --tags Key=Name,Value=$STACK_NAME-csye6225-vpc
+aws ec2 create-tags --resources $VPC_ID --tags Key=Name,Value=$STACK_NAME
+
+aws ec2 create-internet-gateway
