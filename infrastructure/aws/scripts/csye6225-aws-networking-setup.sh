98b37b35f5990f41be6b802209cc3c77da8777a0
zhenyu <zhu.zheny@husky.neu.edu>
2018-01-30 16:52:39 -0500

    attachgateway

--- infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
@@ -5,4 +5,6 @@ STACK_NAME=$1
 VPC_ID=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 | jq -r '.Vpc.VpcId')
 aws ec2 create-tags --resources $VPC_ID --tags Key=Name,Value=$STACK_NAME
 
-aws ec2 create-internet-gateway
+GATEWAY_ID=$(aws ec2 create-internet-gateway | jq -r '.InternetGateway.InternetGatewayId')
+
+aws ec2 attach-internet-gateway --internet-gateway-id $GATEWAY_ID --vpc-id $VPC_ID
