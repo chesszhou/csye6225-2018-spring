d5b4891c64e2c666e07d6c9225634b7137f83178
zhenyu <zhu.zheny@husky.neu.edu>
2018-01-31 15:33:07 -0500

    delete completed

--- infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
@@ -6,7 +6,11 @@ VPC_ID=$(aws ec2 describe-vpcs --filters Name=tag:Name,Values=$STACK_NAME-csye62
 
 ROUTE_TABLE_ID=$(aws ec2 describe-route-tables --filters Name=tag:Name,Values=$STACK_NAME-csye6225-public-route-table | jq -r '.RouteTables[0].RouteTableId')
 
+SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --filters Name=vpc-id,Values=$VPC_ID | jq -r '.SecurityGroups[0].GroupId')
+
 GATEWAY_ID=$(aws ec2 describe-internet-gateways --filters Name=tag:Name,Values=$STACK_NAME-csye6225-InternetGateway | jq -r '.InternetGateways[0].InternetGatewayId')
+
+aws ec2 detach-internet-gateway --internet-gateway-id $GATEWAY_ID --vpc-id $VPC_ID
 
 aws ec2 delete-route-table --route-table-id $ROUTE_TABLE_ID
 
