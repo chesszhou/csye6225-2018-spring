a3632f80bba9eb3d3da65ab5ce06ece8dec080c8
zhenyu <zhu.zheny@husky.neu.edu>
2018-01-30 19:53:22 -0500

    delete unfinished

---
+++ infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+STACK_NAME=$1
+
+VPC_ID=$(aws ec2 describe-vpcs --filters Name=tag:Name,Values=$STACK_NAME-csye6225-vpc | jq -r '.Vpcs[0].VpcId')
+
+ROUTE_TABLE_ID=$(aws ec2 describe-route-tables --filters Name=tag:Name,Values=$STACK_NAME-csye6225-public-route-table | jq -r '.RouteTables[0].RouteTableId')
+
+GATEWAY_ID=$(aws ec2 describe-internet-gateways --filters Name=tag:Name,Values=$STACK_NAME-csye6225-InternetGateway | jq -r '.InternetGateways[0].InternetGatewayId')
+
+aws ec2 delete-route-table --route-table-id $ROUTE_TABLE_ID
+
+aws ec2 delete-internet-gateway --internet-gateway-id $GATEWAY_ID
+
+aws ec2 delete-vpc --vpc-id $VPC_ID
