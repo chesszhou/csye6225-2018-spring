0aab931c90d26980c563b21ddf9a89a329f207cf
zhenyu <zhu.zheny@husky.neu.edu>
2018-02-01 15:29:14 -0500

    indicate success

--- infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
@@ -1,19 +1,31 @@
-#!/bin/bash
+#!/bin/sh
 
 STACK_NAME=$1
 
+VPC_ID=1
+
+GATEWAY_ID=1
+
+ROUTE_TABLE_ID=1
+
 VPC_ID=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 | jq -r '.Vpc.VpcId')
 
-aws ec2 create-tags --resources $VPC_ID --tags Key=Name,Value=$STACK_NAME-csye6225-vpc
+if [ "$VPC_ID" != 1 ]; then
+  #statements
+  aws ec2 create-tags --resources $VPC_ID --tags Key=Name,Value=$STACK_NAME-csye6225-vpc
+  GATEWAY_ID=$(aws ec2 create-internet-gateway | jq -r '.InternetGateway.InternetGatewayId')
+fi
 
-GATEWAY_ID=$(aws ec2 create-internet-gateway | jq -r '.InternetGateway.InternetGatewayId')
+if [ "$GATEWAY_ID" != 1 ]; then
+  #statements
+  aws ec2 create-tags --resources $GATEWAY_ID --tags Key=Name,Value=$STACK_NAME-csye6225-InternetGateway
+  aws ec2 attach-internet-gateway --internet-gateway-id $GATEWAY_ID --vpc-id $VPC_ID
+  ROUTE_TABLE_ID=$(aws ec2 create-route-table --vpc-id $VPC_ID | jq -r '.RouteTable.RouteTableId')
+fi
 
-aws ec2 create-tags --resources $GATEWAY_ID --tags Key=Name,Value=$STACK_NAME-csye6225-InternetGateway
-
-aws ec2 attach-internet-gateway --internet-gateway-id $GATEWAY_ID --vpc-id $VPC_ID
-
-ROUTE_TABLE_ID=$(aws ec2 create-route-table --vpc-id $VPC_ID | jq -r '.RouteTable.RouteTableId')
-
-aws ec2 create-tags --resources $ROUTE_TABLE_ID --tags Key=Name,Value=$STACK_NAME-csye6225-public-route-table
-
-aws ec2 create-route --route-table-id $ROUTE_TABLE_ID --destination-cidr-block 0.0.0.0/0 --gateway-id $GATEWAY_ID
+if [ "$ROUTE_TABLE_ID" != 1 ]; then
+  #statements
+  aws ec2 create-tags --resources $ROUTE_TABLE_ID --tags Key=Name,Value=$STACK_NAME-csye6225-public-route-table
+  aws ec2 create-route --route-table-id $ROUTE_TABLE_ID --destination-cidr-block 0.0.0.0/0 --gateway-id $GATEWAY_ID
+  echo "Sucess!"
+fi
