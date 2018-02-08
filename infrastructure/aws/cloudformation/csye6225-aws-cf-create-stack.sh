be23b9604f26cdc1f237e678d8bde003af1bf226
gbwzzy218 <zhuzhenyu1994@hotmail.com>
2018-02-08 16:08:23 -0500

    Update csye6225-aws-cf-create-stack.sh

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -2,11 +2,11 @@
 #!/bin/sh
 
 STACK_NAME=$1
-SUCCESS=1
+SUCCESS=""
 
 SUCCESS=$(aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-networking.json)
 
-if [ "$SUCCESS" != 1 ]; then
+if [ ! -z "$SUCCESS" ]; then
   #statements
   echo "Success!"
 else
