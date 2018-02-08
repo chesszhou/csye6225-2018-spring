6fbff5e6c9aad20ef67338ae5a693a541c46f7b5
zhenyu <zhu.zheny@husky.neu.edu>
2018-02-08 15:14:37 -0500

    conflict cf

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -1,28 +1,14 @@
-<<<<<<< HEAD
 
-#!/bin/sh
-
-STACK_NAME=$1
-
-SUCCESS=1
-
-SUCCESS=$(aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-networking.json --parameters ParameterKey=StackName,ParameterValue=$STACK_NAME)
-=======
 #!/bin/sh
 
 STACK_NAME=$1
 SUCCESS=1
 
 SUCCESS=$(aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-networking.json)
->>>>>>> 5687c821f56c3248dc145fa6dd9252cf1005409c
 
 if [ "$SUCCESS" != 1 ]; then
   #statements
   echo "Success!"
 else
-<<<<<<< HEAD
-  echo" Failed!"
-=======
   echo "Failed!"
->>>>>>> 5687c821f56c3248dc145fa6dd9252cf1005409c
 fi
--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 STACK_NAME=$1
-<<<<<<< HEAD
 
-=======
->>>>>>> 5687c821f56c3248dc145fa6dd9252cf1005409c
 SUCCESS=1
 
 SUCCESS=$(aws cloudformation delete-stack --stack-name $STACK_NAME)
