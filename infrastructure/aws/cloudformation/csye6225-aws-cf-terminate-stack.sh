a22e4a5cacd5a4d376b1050e86d09fdadc47976f
gbwzzy218 <zhuzhenyu1994@hotmail.com>
2018-02-08 16:10:07 -0500

    Update csye6225-aws-cf-terminate-stack.sh

--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
@@ -4,9 +4,11 @@ STACK_NAME=$1
 
 SUCCESS=1
 
+STATUS=1
+
 SUCCESS=$(aws cloudformation delete-stack --stack-name $STACK_NAME)
 
-if [ -z "$SUCCESS" ]; then
+if [ -z "$STATUS" ]; then
   #statements
   echo "Success"
 else
