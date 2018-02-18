4cb5a3b1f22533dc2c9f032fe3bf7702d3e2520f
zhenyu <zhu.zheny@husky.neu.edu>
2018-02-18 01:41:24 -0500

    scripts

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -3,9 +3,11 @@
 STACK_NAME=$1
 SUCCESS=""
 WAIT=""
-SUBNET_NAME_DB=""
-EC2_SUBNET_ID=""
-EC2_SG_ID=""
+SUBNET_NAME_DB="subnet-group"
+EC2_SUBNET_ID=$(aws ec2 describe-subnets --filters Name=tag:aws:cloudformation:logical-id,Values=SubnetForWebServers | jq -r '.Subnets[0].SubnetId')
+EC2_SG_ID=$(aws ec2 describe-security-groups --filters Name=tag:aws:cloudformation:logical-id,Values=csye6225webapp | jq -r '.SecurityGroups.GroupId')
+
+
 
 SUCCESS=$(aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-application.json --parameters ParameterKey=IdOfImage,ParameterValue=ami-66506c1c ParameterKey=TypeOfInstance,ParameterValue=t2.micro ParameterKey=NameOfBucket,ParameterValue=s3.csye6225-spring2018-zhuzheny.me ParameterKey=ClassofDBInstance,ParameterValue=db.t2.medium ParameterKey=IdentiferofDBinstance,ParameterValue=csye6225-spring2018 ParameterKey=NameofDB,ParameterValue=csye6225 ParameterKey=usernameOfMaster,ParameterValue=csye6225master  ParameterKey=passwordOfMaster,ParameterValue=csye6225password ParameterKey=TypeofEngine,ParameterValue=MySQL ParameterKey=versionOfEngine,ParameterValue=5.6.37 ParameterKey=SubnetGroupNameofDB,ParameterValue=$SUBNET_NAME_DB ParameterKey=IdofEC2Subnet,ParameterValue=$EC2_SUBNET_ID ParameterKey=IdofEC2SecurityGroups,ParameterValue=$EC2_SG_ID)
 
