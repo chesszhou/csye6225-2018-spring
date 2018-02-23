1f77104b9428fdcb06a40c618a35c70737e84c2b
zhenyu <zhu.zheny@husky.neu.edu>
2018-02-23 04:07:26 -0500

    dawn

--- webapp/src/main/java/com/csye6225/spring2018/controller/UploadController.java
+++ webapp/src/main/java/com/csye6225/spring2018/controller/UploadController.java
@@ -5,6 +5,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.iterable.S3Objects;
 import com.amazonaws.services.s3.model.*;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
@@ -28,6 +29,7 @@ public class UploadController {
     public String handleFileUpload(@RequestParam("content") String content,
                                    @RequestParam("filechooser") MultipartFile file,
                                    @RequestParam("username") String username,
+                                   @RequestParam("picURL") String picURL,
                                    RedirectAttributes redirectAttributes, Model model) {
         AWSCredentials credentials = new BasicAWSCredentials("AKIAJYRJRH6MYNFWM5CA", "Je05pI284KdSIZj2zlyL3QrPh1PPX+u+Fy16la18");
         AmazonS3 s3client = new AmazonS3Client(credentials);
@@ -41,17 +43,37 @@ public class UploadController {
            extension = fileName.substring(i+1);
         }
 
-        String bucketName = "s3.csye6225-spring2018-zhuzheny.me";
+        String bucketName = "s3.csye6225-spring2018-profilepics.me";
         s3client.createBucket(bucketName);
         String picName = username + "." + extension;
 
         model.addAttribute("time", new Date());
         model.addAttribute("username", username);
 
+        String toRetrieve = "";
+        S3Object retrievedPic = null;
+
+
         try{
             InputStream is = file.getInputStream();
             //save on s3 with public read access
             s3client.putObject(new PutObjectRequest(bucketName, picName, is, new ObjectMetadata()).withCannedAcl(CannedAccessControlList.PublicRead));
+            for(S3ObjectSummary summary: S3Objects.inBucket(s3client, bucketName)){
+                String repicName = summary.getKey();
+                int j = repicName.lastIndexOf('.');
+                String ownerName = picName.substring(0, j);
+                if(ownerName.equals(username)){
+                    toRetrieve = picName;
+                    retrievedPic = s3client.getObject(bucketName, toRetrieve);
+                    break;
+                }
+            }
+            if(retrievedPic != null){
+                picURL = retrievedPic.getObjectContent().getHttpRequest().getURI().toString();
+                model.addAttribute("picURL",retrievedPic.getObjectContent().getHttpRequest().getURI().toString());
+            }else {
+                model.addAttribute("picURL", "http://via.placeholder.com/350x150");
+            }
             //get a reference to the image object
             S3Object s3object = s3client.getObject(new GetObjectRequest(
                     bucketName, picName));
