b11e386e3373d8a9acaa0a45aba7a55a02eac60d
zhenyu <zhu.zheny@husky.neu.edu>
2018-02-23 01:47:07 -0500

    delete

--- webapp/src/main/java/com/csye6225/spring2018/controller/UploadController.java
+++ webapp/src/main/java/com/csye6225/spring2018/controller/UploadController.java
@@ -7,6 +7,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.UUID;
 
 
@@ -23,8 +25,8 @@ public class UploadController {
     @RequestMapping(method = RequestMethod.POST, value = "/loggedin")
     public String handleFileUpload(@RequestParam("content") String content,
                                    @RequestParam("filechooser") MultipartFile file,
-//                                   @RequestParam("username") String username,
-                                   RedirectAttributes redirectAttributes) {
+                                   @RequestParam("username") String username,
+                                   RedirectAttributes redirectAttributes, Model model) {
         AWSCredentials credentials = new BasicAWSCredentials("AKIAJYRJRH6MYNFWM5CA", "Je05pI284KdSIZj2zlyL3QrPh1PPX+u+Fy16la18");
         AmazonS3 s3client = new AmazonS3Client(credentials);
 
@@ -39,7 +41,10 @@ public class UploadController {
 
         String bucketName = "s3.csye6225-spring2018-zhuzheny.me";
         s3client.createBucket(bucketName);
-        String picName = "ahahha" + "." + extension;
+        String picName = username + "." + extension;
+
+        model.addAttribute("time", new Date());
+        model.addAttribute("username", username);
 
         try{
             InputStream is = file.getInputStream();
