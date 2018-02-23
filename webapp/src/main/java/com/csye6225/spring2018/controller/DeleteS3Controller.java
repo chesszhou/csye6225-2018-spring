77f4e59a57e01ad39bbdf5ab65e013156d1c3c50
周奕弈 <zhou.yiy@husky.neu.edu>
2018-02-23 13:48:38 -0500

    s3

--- webapp/src/main/java/com/csye6225/spring2018/controller/DeleteS3Controller.java
+++ webapp/src/main/java/com/csye6225/spring2018/controller/DeleteS3Controller.java
@@ -24,7 +24,7 @@ public class DeleteS3Controller {
         AWSCredentials credentials = new BasicAWSCredentials("AKIAJYRJRH6MYNFWM5CA", "Je05pI284KdSIZj2zlyL3QrPh1PPX+u+Fy16la18");
         AmazonS3 s3client = new AmazonS3Client(credentials);
         String toDelete = "";
-        for(S3ObjectSummary summary: S3Objects.inBucket(s3client, "s3.csye6225-spring2018-zhuzheny.me")){
+        for(S3ObjectSummary summary: S3Objects.inBucket(s3client, "s3.csye6225-spring2018-profilepics.me")){
             String picName = summary.getKey();
             int i = picName.lastIndexOf('.');
             String ownerName = picName.substring(0, i);
@@ -34,7 +34,7 @@ public class DeleteS3Controller {
             }
         }
         if(!toDelete.equals("")){
-            s3client.deleteObject("s3.csye6225-spring2018-zhuzheny.me", toDelete);
+            s3client.deleteObject("s3.csye6225-spring2018-profilepics.me", toDelete);
         }
         model.addAttribute("time", new Date());
         model.addAttribute("username", userName);
