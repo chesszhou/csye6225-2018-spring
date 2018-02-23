7c2a78ef2da9a7146b8b639e77042d43e33b15cc
周奕弈 <zhou.yiy@husky.neu.edu>
2018-02-22 22:31:22 -0500

    web

--- webapp/src/main/java/com/csye6225/spring2018/controller/UploadController.java
+++ webapp/src/main/java/com/csye6225/spring2018/controller/UploadController.java
@@ -1,5 +1,9 @@
 package com.csye6225.spring2018.controller;
 
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,11 +18,13 @@ import java.util.UUID;
 
 @Controller
 public class UploadController {
-    @RequestMapping(method = RequestMethod.POST, value = "/uploadSuc")
-    public String handleFileUpload(@RequestParam("about") String about,
+    @RequestMapping(method = RequestMethod.POST, value = "/loggedin")
+    public String handleFileUpload(@RequestParam("content") String content,
                                    @RequestParam("file") MultipartFile file,
+                                   @RequestParam("username") String username,
                                    RedirectAttributes redirectAttributes) {
           AmazonS3 s3client = new AmazonS3Client(new ProfileCredentialsProvider());
+
 
           String bucketName = "renda-" + UUID.randomUUID();
           s3client.createBucket(bucketName);
@@ -27,20 +33,20 @@ public class UploadController {
               InputStream is = file.getInputStream();
 
               //save on s3 with public read access
-              s3client.putObject(new PutObjectRequest(bucketName, about, is, new ObjectMetadata()).withCannedAcl(CannedAccessControlList.PublicRead));
+              s3client.putObject(new PutObjectRequest(username, content, is, new ObjectMetadata()).withCannedAcl(CannedAccessControlList.PublicRead));
 
               //get a reference to the image object
               S3Object s3object = s3client.getObject(new GetObjectRequest(
-                      bucketName, about));
+                      username, content));
 
               //add to model
-              redirectAttributes.addAttribute("picUrl", s3object.getObjectContent().getHttpRequest().getURI.toString();
+              RedirectAttributes picUrl = redirectAttributes.addAttribute("picUrl", s3object.getObjectContent().getHttpRequest().getURI().toString());
 
-              System.out.println(s3object.getObjectContent().getHttpRequest().getURI.toString());
+              System.out.println(s3object.getObjectContent().getHttpRequest().getURI().toString());
           } catch (IOException e) {
               e.printStackTrace();
           }
 
-          return ""
+          return "loggedin";
     }
 }
