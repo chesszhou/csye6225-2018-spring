f863e1813702b1c05ba3c3717ef6de6d920b2cc8
beiwen <guo.bei@husky.neu.edu>
2018-02-26 16:59:57 -0500

    fix shell scripts

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-ci-cd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-ci-cd-stack.sh
@@ -5,7 +5,7 @@ STACK_NAME=$1
 SUCCESS=""
 WAIT=""
 
-SUCCESS=$(aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-ci-cd.json --parameters ParameterKey=userName,ParameterValue=travis ParameterKey=nameOfBucket,ParameterValue=code-deploy.csye6225-spring2018-guobei.me ParameterKey=nameOfCodeDeployApplication,ParameterValue=csye6225-spring-2018)
+SUCCESS=$(aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-ci-cd.json --parameters ParameterKey=userName,ParameterValue=travis ParameterKey=nameOfBucket,ParameterValue=code-deploy.csye6225-spring2018-guobei.me ParameterKey=nameOfCodeDeployApplication,ParameterValue=csye6225-spring-2018 --capabilities CAPABILITY_NAMED_IAM)
 WAIT=$(aws cloudformation wait stack-create-complete --stack-name $STACK_NAME)
 
 if [ ! -z "$SUCCESS" ] && [ -z "$WAIT" ]; then
