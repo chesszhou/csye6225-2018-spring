7355246366b81386e7d7c318f90fa004decf6d5e
gbwzzy218 <zhuzhenyu1994@hotmail.com>
2018-02-28 02:11:33 -0500

    change permission

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # update the permission and ownership of WAR file in the tomcat webapps directory
-pwd
+cd /var/lib/tomcat8/webapps
 sudo chown tomcat8:tomcat8 ROOT.war
 sudo chmod 777 ROOT.war
