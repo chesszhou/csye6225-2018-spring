d37f42f3e44f565919584ccc17961d63f39065d7
beiwen <guo.bei@husky.neu.edu>
2018-04-14 19:07:08 -0400

    add security group for load balancer

--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-application-stack.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 STACK_NAME=$1
-SUCCESS=1
-EC2INSTANCEID=""
 STATUS=1
 
-EC2INSTANCEID=$(aws ec2 describe-instances --filters Name=tag:Name,Values=csye6225-EC2Instance | jq -r '.Reservations[].Instances[].InstanceId')
-if [ -z "$EC2INSTANCEID" ]; then
-  echo "The stack you entered does not exist or there are no ec2 instance in entered stack!"
+STATUS=$(aws cloudformation describe-stacks --stack-name $STACK_NAME)
+
+if [ -z "$STATUS" ]; then
+  echo "The stack you entered does not exist!"
 else
-  aws ec2 modify-instance-attribute --instance-id $EC2INSTANCEID --no-disable-api-termination
+  # aws ec2 modify-instance-attribute --instance-id $EC2INSTANCEID --no-disable-api-termination
+  aws s3 rm s3://s3.csye6225-spring2018-guobei.me --recursive
   aws cloudformation delete-stack --stack-name $STACK_NAME
   aws cloudformation wait stack-delete-complete --stack-name $STACK_NAME
   STATUS=$(aws cloudformation describe-stacks --stack-name $STACK_NAME)
--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 STACK_NAME=$1
-
-SUCCESS=1
-
 STATUS=1
-
-SUCCESS=$(aws cloudformation delete-stack --stack-name $STACK_NAME)
-
-aws cloudformation wait stack-delete-complete --stack-name $STACK_NAME
 
 STATUS=$(aws cloudformation describe-stacks --stack-name $STACK_NAME)
 
 if [ -z "$STATUS" ]; then
-  #statements
-  echo "Success"
+  echo "The stack you entered does not exist!"
 else
-  echo "Failed"
+  aws cloudformation delete-stack --stack-name $STACK_NAME
+  aws cloudformation wait stack-delete-complete --stack-name $STACK_NAME
+  STATUS=$(aws cloudformation describe-stacks --stack-name $STACK_NAME)
+  if [ -z "$STATUS" ]; then
+    #statements
+    echo "Success"
+  else
+    echo "Failed"
+  fi
 fi
