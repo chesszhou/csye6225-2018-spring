4ffb6ab0bb0ec04502bf347bc83db777f84bfff3
chesszhou <33828179+chesszhou@users.noreply.github.com>
2018-04-14 21:27:54 -0400

    Update csye6225-aws-cf-create-ci-cd-stack.sh

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-ci-cd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-ci-cd-stack.sh
@@ -5,7 +5,7 @@ STACK_NAME=$1
 STACKID=""
 STATUS=""
 
-STACKID=$(aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-ci-cd.json --parameters ParameterKey=userName,ParameterValue=travis ParameterKey=nameOfBucket,ParameterValue=code-deploy.csye6225-spring2018-zhenyu.me ParameterKey=nameOfCodeDeployApplication,ParameterValue=csye6225-spring-2018 ParameterKey=nameOfDeploymentGroup,ParameterValue=csye6225-deployment-group ParameterKey=TagKey,ParameterValue=Name ParameterKey=TagValue,ParameterValue=csye6225-EC2Instance ParameterKey=TargetGroupName,ParameterValue=MyTargetGroup --capabilities CAPABILITY_NAMED_IAM)
+STACKID=$(aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-ci-cd.json --parameters ParameterKey=userName,ParameterValue=travis ParameterKey=nameOfBucket,ParameterValue=code-deploy.csye6225-spring2018-zhouyiy.me ParameterKey=nameOfCodeDeployApplication,ParameterValue=csye6225-spring-2018 ParameterKey=nameOfDeploymentGroup,ParameterValue=csye6225-deployment-group ParameterKey=TagKey,ParameterValue=Name ParameterKey=TagValue,ParameterValue=csye6225-EC2Instance ParameterKey=TargetGroupName,ParameterValue=MyTargetGroup --capabilities CAPABILITY_NAMED_IAM)
 aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
 STATUS=$(aws cloudformation describe-stack-events --stack-name $STACK_NAME | jq -r '.StackEvents[0].ResourceStatus')
 
